571ff15e845074a1946134777b274271f21913d2
unknown <elias78abcd@gmail.com>
2023-04-22 22:56:39 +0200

    changement de texte

--- src/section/AboutSection.js
+++ src/section/AboutSection.js
@@ -9,17 +9,17 @@ const AboutSection = () => {
             </div>
             <div className={style["right-content-about"]}>
                 <p className={style["about-text"]}>
-                    Je suis actuellement à la recherche d'une alternance pour compléter mon Bachelor en Web Development à l'EEMI.
-                    Le rythme est d'une semaine à l'école et trois semaines en entreprise.
-                    Habitant à Noisy-le-roi (Yvelines), il est préférable pour moi de trouver une alternance vers Paris Ouest.
+                    Actuellement étudiant en Bachelor en Développement Web à l'Ecole Européenne des Métiers de l'Internet (EEMI), 
+                    je suis à la recherche d'une alternance en région parisienne. 
+                    Cette alternance comprend une semaine de cours à l'école et trois semaines en entreprise.
                     <br/>
                     <br/>
-                    Je suis toujours ouvert à de nouvelles opportunités pour me forger de l'expérience dans les domaines du Web Development
-                    et de l'UI UX Design. Je suis super curieux alors j'aimerais explorer les deux domaines qui me plaisent pour être
-                    polyvalent. Je m'intéresse aussi à la culture niponne, aux jeux-vidéos ainsi qu'au basket-ball.
+                    Je suis toujours ouvert à de nouvelles opportunités pour aquérir de l'expérience dans les domaines du Web Development
+                    et de l'UI UX Design. J'ai une grande curiosité d'esprit et souhaite explorer ces deux domaines pour être polyvalent. 
+                    Je m'intéresse aussi à la culture niponne, aux jeux-vidéo et au basket-ball.
                     <br/>
                     <br/>
-                    Alors si vous êtes à la recherche de quelqu'un de curieux et créatif, contactez-moi !
+                    Si vous êtes à la recherche de quelqu'un de curieux et créatif, contactez-moi !
                 </p>
             </div>
         </div>
